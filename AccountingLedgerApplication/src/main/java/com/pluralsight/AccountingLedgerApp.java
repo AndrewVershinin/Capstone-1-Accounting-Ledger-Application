1ff0b00669951c582b3b467ec6237b3170a2172d
Anrew <andrvolg.job@gmail.com>
2025-04-28 11:02:36 -0400

    added 4 classes and transaction.csv in resourses. The project structor is done

--- AccountingLedgerApplication/src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class AccountingLedgerApp {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+    }
 }
---
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/TransactionFileManager.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+import java.util.List;
+
+// this class does reading and writing to transactions.csv
+public class TransactionFileManager {
+
+    public static List<Transaction> loadTransactions(String filename) {
+        // read from csv and return List<Transaction>
+    }
+
+    public static void saveTransaction(String filename, Transaction transaction) {
+        // add a transaction to the CSV file
+    }
+
+}
