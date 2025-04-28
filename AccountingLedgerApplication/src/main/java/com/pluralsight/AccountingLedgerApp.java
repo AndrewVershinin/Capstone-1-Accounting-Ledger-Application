33d72a39ccfda45b1df5539d86a3ef144fe3cd0f
Anrew <andrvolg.job@gmail.com>
2025-04-28 11:37:47 -0400

    added the writer saveTransaction() and tested it

--- AccountingLedgerApplication/src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,10 +1,25 @@
 package com.pluralsight;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
+        // test transaction
+        Transaction t = new Transaction(
+                LocalDate.now(),
+                LocalTime.now(),
+                "Deposit from somewhere",
+                "ATM",
+                new BigDecimal("1000.00")
+
+        );
+
+        TransactionFileManager.saveTransaction("src/main/resources/transactions.csv", t);
     }
 }
--- AccountingLedgerApplication/src/main/java/com/pluralsight/TransactionFileManager.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/TransactionFileManager.java
@@ -1,15 +1,35 @@
 package com.pluralsight;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
 
 // this class does reading and writing to transactions.csv
 public class TransactionFileManager {
 
-    public static List<Transaction> loadTransactions(String filename) {
-        // read from csv and return List<Transaction>
-    }
+//    public static List<Transaction> loadTransactions(String filename) {
+//        // read from csv and return List<Transaction>
+//
+//    }
 
     public static void saveTransaction(String filename, Transaction transaction) {
         // add a transaction to the CSV file
+        try {
+            BufferedWriter bufWriter = new BufferedWriter(new FileWriter(filename, true));
+
+            String line = transaction.getDate() + "|" +
+                            transaction.getTime() + "|" +
+                            transaction.getDescription() + "|" +
+                            transaction.getVendor() + "|" +
+                            transaction.getAmount() + "|";
+
+            bufWriter.write(line);
+            bufWriter.newLine();
+            bufWriter.close();
+        } catch (IOException e) {
+            System.out.println("Am error happened while saving the transaction.");
+            e.printStackTrace();
+        }
     }
 
 }
