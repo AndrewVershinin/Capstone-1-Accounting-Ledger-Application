efc8fd38c2ee9d81294a1e49b190f58c59774e97
Anrew <andrvolg.job@gmail.com>
2025-04-29 09:24:19 -0400

    added cases in reports menu (with no full logic)

--- AccountingLedgerApplication/src/main/java/com/pluralsight/Ledger.java
+++ AccountingLedgerApplication/src/main/java/com/pluralsight/Ledger.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Scanner;
 
 public class Ledger {
 
@@ -37,6 +38,49 @@ public class Ledger {
             if (t.getAmount().compareTo(BigDecimal.ZERO) < 0) {
                 System.out.println(t.getDate() + " " + t.getTime() + " | " + t.getDescription() + " | " + t.getVendor() + " | " + t.getAmount());
             }
+        }
+    }
+
+    public void showReports() {
+        // for reports logic
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("\nReports Menu");
+            System.out.println("1. Month-To-Date");
+            System.out.println("2. Previous Month");
+            System.out.println("3. Year-To-Date");
+            System.out.println("4. Previous Year");
+            System.out.println("5. Search by Vendor");
+            System.out.println("0. Back");
+            System.out.print("Enter selection: ");
+
+            int userChoice = input.nextInt();
+            input.nextLine();
+
+            switch (userChoice) {
+                case 1:
+                    System.out.println("Month To Date");
+                    break;
+                case 2:
+                    System.out.println("Previous Month");
+                    break;
+                case 3:
+                    System.out.println("Year To Date");
+                    break;
+                case 4:
+                    System.out.println("Previous Year");
+                    break;
+                case 5:
+                    System.out.print("Enter vendor name to search: ");
+                    String vendor = input.nextLine();
+                    System.out.println("Search results for vendor: " + vendor);
+                    break;
+                case 0:
+                    return; // exists and goes back to the ledger menu
+            }
+
+
         }
     }
 
